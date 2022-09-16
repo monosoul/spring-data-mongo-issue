6be0792ce75373ad5f11d2bfc56237e68a004441
monosoul <Kloz.Klaud@gmail.com>
2022-09-16 15:32:57 +0200

    Configure mongo type mapper explicitly

--- src/main/kotlin/dev/monosoul/spring/data/mongo/issue/MongoDbConfig.kt
+++ src/main/kotlin/dev/monosoul/spring/data/mongo/issue/MongoDbConfig.kt
@@ -4,16 +4,26 @@ import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import dev.monosoul.spring.data.mongo.issue.FieldType.FieldTypeImpl
+import dev.monosoul.spring.data.mongo.issue.FieldType.OtherFieldTypeImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.convert.ConfigurableTypeInformationMapper
+import org.springframework.data.convert.SimpleTypeInformationMapper
+import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper.DEFAULT_TYPE_KEY
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 @EnableMongoRepositories(
     basePackageClasses = [DocumentRepository::class]
 )
@@ -22,6 +32,30 @@ class MongoDbConfig : AbstractMongoClientConfiguration() {
 
     @Autowired
     private lateinit var mongoProperties: MongoProperties
+
+    @Bean
+    fun configurableTypeInformationMapper() = ConfigurableTypeInformationMapper(
+        mapOf(
+            FieldTypeImpl::class.java to "field_type_impl",
+            OtherFieldTypeImpl::class.java to "other_field_type_impl",
+        )
+    )
+
+    override fun mappingMongoConverter(
+        databaseFactory: MongoDatabaseFactory,
+        customConversions: MongoCustomConversions,
+        mappingContext: MongoMappingContext
+    ) = super.mappingMongoConverter(databaseFactory, customConversions, mappingContext).apply {
+        setTypeMapper(
+            DefaultMongoTypeMapper(
+                DEFAULT_TYPE_KEY,
+                listOf(
+                    configurableTypeInformationMapper(),
+                    SimpleTypeInformationMapper(),
+                )
+            )
+        )
+    }
 
     override fun getDatabaseName() = mongoProperties.dbName
 
